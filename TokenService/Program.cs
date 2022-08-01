3878dd153d1975aad1dea6ffe547c00459472660
tatkhanhgia <65067602+tatkhanhgia@users.noreply.github.com>
2022-08-01 11:55:02 +0700

    Update Program mainclasss

--- TokenService/Program.cs
+++ TokenService/Program.cs
@@ -34,28 +34,14 @@ namespace TokenService
             ////string cipher = manageAlgorithm.EncryptPrivateKey(generateKeypair.getPublicKey().ToString())
             //writeExcelFile.ExportExcel(CSR,null);
             //Console.WriteLine("Export Excel Successfully !");
-
-            //String path = @"C:\Users\gia\Desktop\cert.cer";
-            //X509Certificate certificate = Utils.readCertificate(path);            
-            //Console.WriteLine("SubjectDN"+certificate.SubjectDN);
-            //String base64 = Utils.convertCertToBase64(certificate);
-            //Console.WriteLine("Base64:"+base64);
-            //ManageAlgorithm algorithm = new ManageAlgorithm();
-            //String input = "Tat Khanh Giaajlwdh kagoqje lkqhjkdcndHQOIW Dadm nUQIYHD HJKASNK ioqw d";
-            //String key2 = "alo mot hai ba basddasdn nam sau bay tam chin muoiw";
-
-            //String encrypt = algorithm.EncryptString(key2, input);
-            //String decrypt = algorithm.DecryptString(key2, encrypt);
-            //Console.WriteLine("Input:" + input);
-            //Console.WriteLine("Encrypt:" + encrypt);
-            //Console.WriteLine("Decrypt:" + decrypt);
+            
 
 
-            //Console.WriteLine("\n===== Generate PKCS12 Keystore Successfully ! =====");
-            //GenerateP12 generateP12 = new GenerateP12();
+            Console.WriteLine("\n===== Generate PKCS12 Keystore Successfully ! =====");
+            GenerateP12 generateP12 = new GenerateP12();
 
-            //X509Certificate endEntityCert = Utils.readCertificateFromFile("file/certificate.cer");
-            //generateP12.pkcs12Keystore(endEntityCert, generateKeypair.getKey(), "file/testP12.p12", "testP12", "12345678");
+            X509Certificate endEntityCert = Utils.readCertificateFromFile("file/certificate.cer");
+            generateP12.pkcs12Keystore(endEntityCert, generateKeypair.getKey(), "file/testP12.p12", "testP12", "12345678");
 
         }
     }
