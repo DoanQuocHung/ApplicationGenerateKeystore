100d38a58c73067f6483b5aa990e4d4bd33dffd6
tatkhanhgia <65067602+tatkhanhgia@users.noreply.github.com>
2022-08-01 15:44:50 +0700

    Update read file Certificate (PEM - DER)

--- TokenService/Program.cs
+++ TokenService/Program.cs
@@ -13,7 +13,6 @@ namespace TokenService
         static void Main(string[] args)
         {
             ManageKey generateKeypair = new ManageKey();
-
             Console.WriteLine("===== Sinh khóa RSA và sinh CSR =====");
             string subjectDN = generateKeypair.createInformation();
             generateKeypair.generateKey(2048);
@@ -22,28 +21,28 @@ namespace TokenService
             Console.WriteLine("Generated CSR Successfully !");
 
 
-            Console.WriteLine("\n===== Decrypt private key =====");
-            ManageAlgorithm a = new ManageAlgorithm();
-            string result = a.EncryptPrivateKey(generateKeypair.getKey());
-            Console.WriteLine("Encrypt:" + result);
-            string privatekey = a.DecryptPrivateKey(generateKeypair.getPublicKey(), result);
-            Console.WriteLine("\nPrivatekey:" + privatekey);
+            //Console.WriteLine("\n===== Decrypt private key =====");
+            //ManageAlgorithm a = new ManageAlgorithm();
+            //string result = a.EncryptPrivateKey(generateKeypair.getKey());
+            //Console.WriteLine("Encrypt:" + result);
+            //string privatekey = a.DecryptPrivateKey(generateKeypair.getPublicKey(), result);
+            //Console.WriteLine("\nPrivatekey:" + privatekey);
 
 
-            Console.WriteLine("\n===== Export Excel File =====");
-            WriteExcelFile writeExcelFile = new WriteExcelFile();
-            //Cipher privateKey
-            //ManageAlgorithm manageAlgorithm = new ManageAlgorithm();
-            //string cipher = manageAlgorithm.EncryptPrivateKey(generateKeypair.getPublicKey().ToString())
-            writeExcelFile.ExportExcel(CSR, null);
-            Console.WriteLine("Export Excel Successfully !");
+            //Console.WriteLine("\n===== Export Excel File =====");
+            //WriteExcelFile writeExcelFile = new WriteExcelFile();
+            ////Cipher privateKey
+            ////ManageAlgorithm manageAlgorithm = new ManageAlgorithm();
+            ////string cipher = manageAlgorithm.EncryptPrivateKey(generateKeypair.getPublicKey().ToString())
+            //writeExcelFile.ExportExcel(CSR, null);
+            //Console.WriteLine("Export Excel Successfully !");
 
 
-            Console.WriteLine("\n===== Generate PKCS12 Keystore =====");
+            //Console.WriteLine("\n===== Generate PKCS12 Keystore =====");
             GenerateP12 generateP12 = new GenerateP12();
             X509Certificate endEntityCert = Utils.readCertificateFromFile("file/cert.cer");
-            X509Certificate CertChain = Utils.readCertificateFromFile("file/mobileidcert.cer");
-
+            //X509Certificate CertChain = Utils.readCertificateFromFile("file/mobileidcert.cer");
+            X509Certificate CertChain = Utils.readCertificateFromFile(@"C:\Users\gia\Desktop\certChain.cer");
             generateP12.pkcs12Keystore(endEntityCert, CertChain, generateKeypair.getKey(), "file/testP12.p12", "testP12", "12345678");
             Console.WriteLine("\nGenerate PKCS12 Keystore Successfully !");
 
