388a3160775b92468cb27383a29863fe58b45e91
Quoc Hung <gamekingdom234@gmail.com>
2022-08-01 14:18:45 +0700

    Update GUI v1.0

--- DesktopApp/MainForm.Designer.cs
+++ DesktopApp/MainForm.Designer.cs
@@ -30,11 +30,25 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1211, 376);
@@ -43,21 +57,107 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label2);
             this.panel2.Location = new System.Drawing.Point(12, 394);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1211, 416);
             this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "YÊU CẦU CẤP PHÁT CHỨNG THƯ SỐ ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(333, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(499, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "YÊU CẦU CẤP PHÁT PKCS12 KEYSTORE\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1023, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cấp phát";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1023, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cấp phát";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(28, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(549, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(304, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Số lượng Chứng thư số yêu cầu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Chọn tệp Excel";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(28, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 57);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Browse File";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(1235, 822);
+            this.ClientSize = new System.Drawing.Size(1235, 816);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.Text = "Tool Keystore Generation";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -66,6 +166,14 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- TokenService/Program.cs
+++ TokenService/Program.cs
@@ -12,36 +12,40 @@ namespace TokenService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("===== Sinh khóa RSA và sinh CSR =====");
             ManageKey generateKeypair = new ManageKey();
+
+            Console.WriteLine("===== Sinh khóa RSA và sinh CSR =====");
             string subjectDN = generateKeypair.createInformation();
             generateKeypair.generateKey(2048);
             //generateKeypair.getPrivateKey();
             string CSR = generateKeypair.generateCSR(subjectDN, null);
             Console.WriteLine("Generated CSR Successfully !");
 
-            //Test en - decrypt private key
+
+            Console.WriteLine("\n===== Decrypt private key =====");
             ManageAlgorithm a = new ManageAlgorithm();
             string result = a.EncryptPrivateKey(generateKeypair.getKey());
-            Console.WriteLine("Encrypt:"+result);
+            Console.WriteLine("Encrypt:" + result);
             string privatekey = a.DecryptPrivateKey(generateKeypair.getPublicKey(), result);
-            Console.WriteLine("\nPrivatekey:"+privatekey);
-
-            //Console.WriteLine("\n===== Export Excel File =====");
-            //WriteExcelFile writeExcelFile = new WriteExcelFile();
-            ////Cipher privateKey
-            ////ManageAlgorithm manageAlgorithm = new ManageAlgorithm();
-            ////string cipher = manageAlgorithm.EncryptPrivateKey(generateKeypair.getPublicKey().ToString())
-            //writeExcelFile.ExportExcel(CSR,null);
-            //Console.WriteLine("Export Excel Successfully !");
-            
+            Console.WriteLine("\nPrivatekey:" + privatekey);
 
 
-            Console.WriteLine("\n===== Generate PKCS12 Keystore Successfully ! =====");
+            Console.WriteLine("\n===== Export Excel File =====");
+            WriteExcelFile writeExcelFile = new WriteExcelFile();
+            //Cipher privateKey
+            //ManageAlgorithm manageAlgorithm = new ManageAlgorithm();
+            //string cipher = manageAlgorithm.EncryptPrivateKey(generateKeypair.getPublicKey().ToString())
+            writeExcelFile.ExportExcel(CSR, null);
+            Console.WriteLine("Export Excel Successfully !");
+
+
+            Console.WriteLine("\n===== Generate PKCS12 Keystore =====");
             GenerateP12 generateP12 = new GenerateP12();
+            X509Certificate endEntityCert = Utils.readCertificateFromFile("file/cert.cer");
+            X509Certificate CertChain = Utils.readCertificateFromFile("file/mobileidcert.cer");
 
-            X509Certificate endEntityCert = Utils.readCertificateFromFile("file/certificate.cer");
-            generateP12.pkcs12Keystore(endEntityCert, generateKeypair.getKey(), "file/testP12.p12", "testP12", "12345678");
+            generateP12.pkcs12Keystore(endEntityCert, CertChain, generateKeypair.getKey(), "file/testP12.p12", "testP12", "12345678");
+            Console.WriteLine("\nGenerate PKCS12 Keystore Successfully !");
 
         }
     }
