cceec7868065106500293d891b5ae1317ddc7c5c
Quoc Hung <gamekingdom234@gmail.com>
2022-08-09 08:43:04 +0700

    Update GUI

--- DesktopApp/MainForm.Designer.cs
+++ DesktopApp/MainForm.Designer.cs
@@ -62,11 +62,11 @@ namespace DesktopApp
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(23, 148);
+            this.label3.Location = new System.Drawing.Point(132, 148);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(255, 22);
+            this.label3.Size = new System.Drawing.Size(146, 22);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Số lượng Chứng thư số yêu cầu";
+            this.label3.Text = "Số lượng yêu cầu";
             // 
             // textBox1
             // 
@@ -93,11 +93,11 @@ namespace DesktopApp
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(352, 30);
+            this.label1.Location = new System.Drawing.Point(466, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(458, 29);
+            this.label1.Size = new System.Drawing.Size(255, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "YÊU CẦU CẤP PHÁT CHỨNG THƯ SỐ ";
+            this.label1.Text = "YÊU CẦU CẤP PHÁT";
             // 
             // panel2
             // 
@@ -149,7 +149,7 @@ namespace DesktopApp
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(139, 43);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Cấp phát P12";
+            this.button2.Text = "Chuyển đổi";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -157,11 +157,11 @@ namespace DesktopApp
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(333, 36);
+            this.label2.Location = new System.Drawing.Point(502, 40);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(499, 29);
+            this.label2.Size = new System.Drawing.Size(172, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "YÊU CẦU CẤP PHÁT PKCS12 KEYSTORE\r\n";
+            this.label2.Text = "CHUYỂN ĐỔI";
             // 
             // MainForm
             // 
@@ -176,7 +176,6 @@ namespace DesktopApp
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Tool Keystore Generation";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
